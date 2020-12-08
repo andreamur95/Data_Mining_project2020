12e2f40e71e7a512c4ac44d55d718be218c101bc
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2020-12-08 21:44:02 +0100

    Datasets, visualization updated, minor bugs fixed, still not working properly

--- spam.py
+++ spam.py
@@ -1,15 +1,27 @@
 
 n_customers = 4   #SID
-n_attributes = 7  #a,b,c,d,e,f
-max_transactions = 10 # number of entries in itemsets cell
+n_attributes = 2  #a,b,c,d,e,f
+max_transactions = 5 # number of entries in itemsets cell
 
 v_dataset = []
 
-for i in range(n_attributes):
-    v_dataset.append([])
-    for j in range(n_customers):
-        v_dataset[i].append([(3*i + 2 * j) % max_transactions])
 
+
+v_dataset.append([]) #item 0
+v_dataset.append([]) #item 1
+
+for i in range(n_customers):
+    v_dataset[0].append([])
+    v_dataset[1].append([])
+
+v_dataset[0][0] = [0,1,2,3]
+v_dataset[0][1] = [0]
+v_dataset[0][2] = [0,1]
+v_dataset[0][3] = [0]
+v_dataset[1][0] = [1,3]
+v_dataset[1][1] = [0,1]
+v_dataset[1][2] = [0,2]
+v_dataset[1][3] = [1]
 
 print(v_dataset)
 
@@ -61,18 +73,17 @@ def spam(dataset, minsup):
     for item_id in range(n_attributes):
         if getSupport(dataset[item_id]) >= minsup:
             frequent_items.append(item_id)
-
     for item in frequent_items:
         try:
-            frequent_items2 = frequent_items
+            frequent_items2 = frequent_items[:] #copying by value;
             frequent_items2.remove(item)
         except ValueError:
             pass
-        pat = [[item]]
-        search(v_dataset[item], pat, frequent_items, frequent_items2, minsup, result)
+        pat = str(item)
+        result = search(dataset[item], pat, frequent_items, frequent_items2, minsup, result)
     return result
 
-# pat is defined vertically
+# v_pat is defined vertically
 def search(v_pat, pat, Sn, In, minsup, mined_sequences):
     mined_sequences.append(pat)
     Stemp = []
@@ -82,30 +93,32 @@ def search(v_pat, pat, Sn, In, minsup, mined_sequences):
             Stemp.append(item)
     for item in Stemp:
         try:
-            Stemp2 = Stemp
+            Stemp2 = Stemp[:] # copying by value
             Stemp2.remove(item)
+            print("s",item, Stemp, Stemp2)
         except ValueError:
             pass
-        pat.append([item])
+        pat += "_"
+        pat += str(item)
         search(s_extension(v_pat, v_dataset[item]),pat , Stemp, Stemp2, minsup, mined_sequences)
 
     for item in In:
-        if getSupport(s_extension(v_pat, v_dataset[item])) >= minsup:
-            Stemp.append(item)
+        if getSupport(i_extension(v_pat, v_dataset[item])) >= minsup:
+            Itemp.append(item)
     for item in Itemp:
+        print(Itemp)
         try:
-            Itemp2 = Stemp
+            Itemp2 = Itemp[:]
             Itemp2.remove(item)
+            print("i",item,Itemp,Itemp2)
         except ValueError:
             pass
-        pat[-1].append(item)
+        pat += str(item)
         search(i_extension(v_pat,v_dataset[item]),pat, Itemp, Itemp2, minsup, mined_sequences)
 
     return mined_sequences
 
-print(spam(v_dataset, 1))
+print(spam(v_dataset, 3))
 
-
-
-
+print(s_extension(s_extension(v_dataset[0],v_dataset[1]),v_dataset[1]))
 
