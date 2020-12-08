6c205b4ae436e8272f96f590280c30a2c9cd03f8
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2020-12-08 01:55:58 +0100

    Code finished, not working properly

--- spam.py
+++ spam.py
@@ -8,7 +8,7 @@ v_dataset = []
 for i in range(n_attributes):
     v_dataset.append([])
     for j in range(n_customers):
-        v_dataset[i].append([(3*i + 2 * j) % max_transactions + 1])
+        v_dataset[i].append([(3*i + 2 * j) % max_transactions])
 
 
 print(v_dataset)
@@ -54,6 +54,56 @@ def i_extension(v_item1, v_item2):
                     result[customer_id].append(entry1)
 
     return result
+
+def spam(dataset, minsup):
+    frequent_items = []
+    result = []
+    for item_id in range(n_attributes):
+        if getSupport(dataset[item_id]) >= minsup:
+            frequent_items.append(item_id)
+
+    for item in frequent_items:
+        try:
+            frequent_items2 = frequent_items
+            frequent_items2.remove(item)
+        except ValueError:
+            pass
+        pat = [[item]]
+        search(v_dataset[item], pat, frequent_items, frequent_items2, minsup, result)
+    return result
+
+# pat is defined vertically
+def search(v_pat, pat, Sn, In, minsup, mined_sequences):
+    mined_sequences.append(pat)
+    Stemp = []
+    Itemp = []
+    for item in Sn:
+        if getSupport(s_extension(v_pat, v_dataset[item])) >= minsup:
+            Stemp.append(item)
+    for item in Stemp:
+        try:
+            Stemp2 = Stemp
+            Stemp2.remove(item)
+        except ValueError:
+            pass
+        pat.append([item])
+        search(s_extension(v_pat, v_dataset[item]),pat , Stemp, Stemp2, minsup, mined_sequences)
+
+    for item in In:
+        if getSupport(s_extension(v_pat, v_dataset[item])) >= minsup:
+            Stemp.append(item)
+    for item in Itemp:
+        try:
+            Itemp2 = Stemp
+            Itemp2.remove(item)
+        except ValueError:
+            pass
+        pat[-1].append(item)
+        search(i_extension(v_pat,v_dataset[item]),pat, Itemp, Itemp2, minsup, mined_sequences)
+
+    return mined_sequences
+
+print(spam(v_dataset, 1))
 
 
 
